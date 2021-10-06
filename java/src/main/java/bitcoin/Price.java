fa132e330376b28d0050c467216a9c493a4178e3
Sara <sluisdejesus@gmail.com>
2021-10-06 22:00:09 +0100

    added getters to Price.java

--- java/src/main/java/bitcoin/Price.java
+++ java/src/main/java/bitcoin/Price.java
@@ -4,4 +4,17 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record Price(LocalDateTime effectiveDate, BigDecimal price) {
+
+    public Price(LocalDateTime effectiveDate, BigDecimal price){
+        this.effectiveDate = effectiveDate;
+        this.price = price;
+    }
+
+    public LocalDateTime getEffectiveDate(){
+        return effectiveDate;
+    }
+
+    public BigDecimal getPrice(){
+        return price;
+    }
 }
