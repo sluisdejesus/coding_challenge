f67600ad4093e8672a09c67ffd32f33831f0ee8f
Sara <sluisdejesus@gmail.com>
2021-10-06 22:00:51 +0100

    added getters to DailyPortfolioValue.java

--- java/src/main/java/bitcoin/DailyPortfolioValue.java
+++ java/src/main/java/bitcoin/DailyPortfolioValue.java
@@ -4,4 +4,17 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record DailyPortfolioValue(LocalDate date, BigDecimal value) {
+
+    public DailyPortfolioValue(LocalDate date, BigDecimal value){
+        this.date = date;
+        this.value = value;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public BigDecimal getValue(){
+        return value;
+    }
 }
