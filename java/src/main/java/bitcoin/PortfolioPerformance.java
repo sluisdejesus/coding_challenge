d10d7f3bfcab414f3589c9b422039727ef0d2ab4
Sara <sluisdejesus@gmail.com>
2021-10-09 15:09:22 +0100

    test now passing, will possibly add more comments to it so I don't forget what's going on
    Have now realised that getters were not needed as this is a new feature of Java 16 but will leave them in for now
    
    This method also needs changing as it assumes that the user will start with 0 Bitcoin and not an existing wallet for the period being looked at.

--- java/src/main/java/bitcoin/PortfolioPerformance.java
+++ java/src/main/java/bitcoin/PortfolioPerformance.java
@@ -1,11 +1,17 @@
 package bitcoin;
 
 import java.math.BigDecimal;
+import java.security.Security;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PortfolioPerformance {
+    private Transaction transaction;
+    private Price price;
 
     private static final List<Price> PRICES = List.of(
             new Price(LocalDateTime.of(2021, Month.SEPTEMBER, 1, 5, 0, 0), new BigDecimal("35464.53")),
@@ -24,6 +30,67 @@ public class PortfolioPerformance {
 
     // Complete this method to return a list of daily portfolio values with one record for each day from the 01-09-2021-07-09-2021 in ascending date order
     public static List<DailyPortfolioValue> getDailyPortfolioValues() {
-        return null;
+
+            //Making a list to hold daily portfolio values to display to user
+            List<DailyPortfolioValue> dailyPortfolioValues = new ArrayList<>();
+
+            // List of dates that need to be calculated (01-09-2021 until but not including 08-09-2021
+            LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 1);
+            LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 8);
+
+            // .datesUntil stays within startDate and endDate
+            // .collect accumulates all date data into a collection
+            List<LocalDate> dates = startDate.datesUntil(endDate).collect(Collectors.toList());
+
+
+
+            // User starts dates with 0 Bitcoin (this will pass test but will need to be amended as user may start
+            // date period with an existing number of Bitcoin in their wallet)
+            BigDecimal currentPrice = null, bitcoinWallet = new BigDecimal("0");
+
+            // Index of transactions and prices to keep track while loooping
+            int purchasePriceIndex = 0, transactionsIndex = 0;
+
+            //For each date in range
+            for (LocalDate date : dates) {
+
+                // Price change - purchasePrice index is smaller than the size of PRICES list, increment index by one
+                for (int index = purchasePriceIndex; index < PRICES.size(); index++) {
+
+                    //If a price change happened on a given day
+                    // get purchasePrice index, .effectiveDate() checks date, .toLocalDate converts from
+                    // LocalDateTime, .equals(date) compares the dates
+                    if (PRICES.get(purchasePriceIndex).effectiveDate().toLocalDate().equals(date)) {
+
+                        // Change price to the new one found
+                        currentPrice = PRICES.get(purchasePriceIndex).price();
+                        purchasePriceIndex++;
+                    }
+                }
+
+                // New transaction
+                // sets index to be the index of the transaction
+                // then we check if the index of the transaction is smaller than the TRANSACTIONS list
+                // if it is, we increment index position by 1
+                for (int index = transactionsIndex; index < TRANSACTIONS.size(); index++) {
+
+                    //If a transaction happened on a given day
+                    // get purchasePrice index, .effectiveDate() checks date, .toLocalDate converts from
+                    // LocalDateTime, .equals(date) compares the dates
+                    if (TRANSACTIONS.get(transactionsIndex).effectiveDate().toLocalDate().equals(date)) {
+
+                        //Update bitcoinWallet by adding new value after adding transaction value
+                        // .add numberOfBitcoins from the transaction at that index position
+                        // then increments the index position by 1
+                        bitcoinWallet = bitcoinWallet.add(TRANSACTIONS.get(transactionsIndex).numberOfBitcoins());
+                        transactionsIndex++;
+                    }
+                }
+
+                // adds new record to List dailyPortfolioValue using the date, bitcoinWallet * currentPrice
+                dailyPortfolioValues.add(new DailyPortfolioValue(date, bitcoinWallet.multiply(currentPrice)));
+
+            }
+            return dailyPortfolioValues;
+        }
     }
-}
